966894dff225ef896684c538f8d11cad971267a0
TP-hoggy077 <hoggy077@gmail.com>
2021-08-26 12:04:27 +1000

    fml

--- Assets/Scripts/CustomTools/RegistryLiveMenu.cs
+++ Assets/Scripts/CustomTools/RegistryLiveMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.EditorTools;
 using UnityEngine;
+using System;
 
 
 public class RegistryLiveMenu : EditorWindow
@@ -25,10 +26,19 @@ public class RegistryLiveMenu : EditorWindow
             }
 
 
-            selected = EditorGUI.Popup(new Rect(0, 0, this.position.width, 20), selected, uIContent.ToArray());
+            selected = EditorGUI.Popup(new Rect(0, 0, this.position.width, (float)(this.position.height * 0.05)), selected, uIContent.ToArray());
             EditorGUILayout.Space();
-            GUILayout.BeginArea(new Rect(0, 25, this.position.width, 50));
-            GUILayout.Button("");
+            GUILayout.BeginArea(new Rect(0, (float)(this.position.height * 0.05) + 1, this.position.width, (float)(this.position.height * 0.05)));
+            //EditorGUI.DrawPreviewTexture(new Rect(0, (float)(this.position.height * 0.05) * 2 + 2, (float)(this.position.width / 0.75), (float)(this.position.height * 0.05)), SpellRegistrySing.Instance.Registry.QueryRegistry(uIContent[selected].text).icon.texture);
+            if (GUILayout.Button("Test fire"))
+                SpellRegistrySing.Instance.Registry.QueryRegistry(uIContent[selected].text).RunFunction(new SpellEffector()
+                {
+                    Name = "TestFire",
+                    FireEffect = new Action<GameObject>((gmeobj) =>
+                    {
+                        Debug.Log($"--Test fire invoked--");
+                    })
+                });
             GUILayout.EndArea();
         }
     }
