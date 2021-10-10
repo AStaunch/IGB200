e3b263f6ea732f68d079e405655ddca0707ab682
AStaunch <79958602+AStaunch@users.noreply.github.com>
2021-10-10 17:55:00 +1000

    ahhhhhhhhhhh

---
+++ Assets/Scripts/Saving/SharedMemory.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static HotbarHandler;
+
+public static class SharedMemory
+{
+    public static Dictionary<AbstractDoor, bool> AllDoors;
+    public static void UpdateDoor(AbstractDoor abstractDoor) {
+        if (!AllDoors.ContainsKey(abstractDoor)) {
+            AllDoors.Add(abstractDoor, abstractDoor.IsOpen);
+        } else {
+            AllDoors[abstractDoor] = abstractDoor.IsOpen;
+        }
+    }
+
+    public static Dictionary<iPhysicsInterface, float[]> AllObjects;
+    public static void UpdateObject(iPhysicsInterface abstractDoor, Vector3 position) {
+        if (!AllObjects.ContainsKey(abstractDoor)) {
+            AllObjects.Add(abstractDoor, new float[] { position.x, position.y, position.z });
+        } else {
+            AllObjects[abstractDoor] = new float[] { position.x, position.y, position.z };
+        }
+    }
+
+    public static PlayerData LastState;
+    public static void UpdatePlayer() {
+
+    }
+
+    public static void SaveToFile() {
+
+    }
+    public static void LoadFromFile() {
+
+    }
+}
+
+public class PlayerData
+{
+    public int Health;
+    public bool[] ElementsUnlocked = new bool[8];
+    public bool[] TemplatesUnlocked = new bool[5];
+    public HotbarItem[] CurrentSpells = new HotbarItem[5];
+}
